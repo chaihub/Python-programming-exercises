54cc9e6385c165c1ab94b726248442f8ffc3cf84
Rajguru Chaitanya <RAJGUCS@asia.ad.whirlpool.com>
2020-11-03 23:53:47 +0530

    Add example 1

--- Ex1.py
+++ Ex1.py
@@ -1,1 +1,9 @@
-print("Hello, world!\n")
+#
+# Find all multiples of 7 in a range that are not divisible by 5
+#
+result=[]
+for num in range(15,72):
+    if (num%7 == 0) and (num%5 != 0):
+        result.append(num)
+
+print(result)
