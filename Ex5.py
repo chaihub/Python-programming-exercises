8c58ce7ea7dc121673db8447f2846387a1f73797
Rajguru Chaitanya <RAJGUCS@asia.ad.whirlpool.com>
2020-11-06 23:04:03 +0530

    Add example 5

---
+++ Ex5.py
@@ -0,0 +1,17 @@
+#
+# Class to read and convert text to uppercase
+#
+class CaseConverter:
+    def __init__(self)
+        self.words = []
+
+    def getString(self):
+        self.words = ['Hasta', 'la', 'vista', 'baby']
+
+    def printString(self):
+        for i in range(len(self.words))
+            self.words[i].upper
+
+test = CaseConverter()
+test.getString()
+test.printString()
