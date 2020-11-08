6bee2841ddfa5901bb84c5b090bdc55df34a9b22
Rajguru Chaitanya <RAJGUCS@asia.ad.whirlpool.com>
2020-11-08 23:13:43 +0530

    Update example 8

--- Ex8.py
+++ Ex8.py
@@ -2,8 +2,7 @@
 # Read two numbers, generate 2D array
 #
 input_str = input()
-print(input_str.split(','))
-rows, cols = input_str.split(',')
-rows, cols = int(rows), int(cols)
-matrix = [[x*y for y in range(cols)] for x in range(rows)]
-print(matrix)
+myList = input_str.split(',')
+myList.sort(key=str.lower, reverse=True)
+# myList = sorted(input_str.split(','))
+print(myList)
