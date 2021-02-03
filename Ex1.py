0a84203ea04f46e63256f7c6bada06e9f33a21f7
Chaitanya Rajguru <33892720+chaihub@users.noreply.github.com>
2021-02-03 14:01:41 +0530

    Extend number range

--- Ex1.py
+++ Ex1.py
@@ -2,7 +2,7 @@
 # Find all multiples of 7 in a range that are not divisible by 5
 #
 result=[]
-for num in range(15,72):
+for num in range(15,92):
     if (num%7 == 0) and (num%5 != 0):
         result.append(num)
 
